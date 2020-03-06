c7e96cbbdff0d5a6a25ab805b64d738daee49161
Restum Ali <alirestum.hu@gmail.com>
2020-03-06 01:38:42 +0100

    Added static HTML pages

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+
+app.use(express.static('public'));
+
+//Setting up server and port number
+const port = 3000;
+const server = app.listen(port, ()=> {
+    console.log('Server is running on port ' + port);
+});
